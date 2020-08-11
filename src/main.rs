38dfdb293ee051a7392bbd22581e80dc6d35fe1a
Louis Bui <louisbui2000@gmail.com>
2020-08-11 15:54:58 -0700

    organized all items a bit

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,58 @@
 use chrono::prelude::*;
-use std::env;
 use std::path::PathBuf;
 use std::process::Command;
+use std::{env, io};
 
-fn main() {
-    // Initialize the general journal path and today's date.
-    let mut base_path = PathBuf::from("/home/longlb/Documents/journal");
+// TODO
+// Opening an entry by specific date
+// Opening a random journal entry
+// Optimization
+// Uses system text editor rather than mousepad?
+// Functionality for fully usable on any system? (Directories and such)
+
+struct Temp {
+    base_path: PathBuf,
+    day: Date<Local>,
+}
+
+impl Temp {
+    fn new() -> Self {
+        Self {
+            base_path: PathBuf::from("/home/longlb/Documents/journal"),
+            day: get_day(),
+        }
+    }
+}
+
+// println!(
+//     "Opening journal entry for {}, {} {}...",
+//     day.year(),
+//     month(day.month()),
+//     day.day()
+// );
+
+fn main() -> io::Result<()> {
+    // Initialize the general journal path and the entry date based on user input..
+    let mut path = PathBuf::from("/home/longlb/Documents/journal");
+    let day = get_day();
+
+    // Enter the day's year and month directories.
+    check_add_dir(&mut path, day.year().to_string())?;
+    check_add_dir(&mut path, month(day.month()))?;
+
+    // Add the day to the path.
+    path.push(day.day().to_string());
+
+    // Execute the combined command.
+    execute("mousepad", path_str(&path))?;
+    Ok(())
+}
+
+// fn main() -> io::Result<()> {}
+
+// Return the day based on the user's input.
+fn get_day() -> Date<Local> {
     let mut day = Local::today();
-
-    // Adjust the day based on the extra arguments given, if there are any.
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
         day = day
@@ -18,39 +62,31 @@ fn main() {
                 _ => chrono::Duration::days(0),
             };
     }
-
-    // Add the year to the path.
-    base_path.push(day.year().to_string());
-    if !base_path.is_dir() {
-        execute("mkdir", base_path.to_str().expect("Not valid"))
-    }
-
-    // Add the month to the path.
-    base_path.push(month(day.month()));
-    if !base_path.is_dir() {
-        execute("mkdir", base_path.to_str().expect("Not valid"))
-    }
-
-    // Add the day to the path.
-    base_path.push(day.day().to_string());
-
-    // Execute the combined command
-    execute("mousepad", base_path.to_str().expect("Not valid"));
-    println!(
-        "Opening journal entry for {}, {} {}...",
-        day.year(),
-        month(day.month()),
-        day.day()
-    );
+    day
 }
 
-fn execute(command: &str, arg: &str) {
-    Command::new(command)
-        .arg(&arg)
-        .spawn()
-        .expect(&format!("Command {} {} failed", command, arg));
+// Attach a directory to the given path and generate it if if doesn't exist.
+fn check_add_dir(path: &mut PathBuf, dir: String) -> io::Result<()> {
+    path.push(dir);
+    if !path.is_dir() {
+        execute("mkdir", path_str(path))?;
+    }
+    Ok(())
 }
 
+// Execute a linux command with a single argument.
+fn execute(command: &str, arg: &str) -> io::Result<()> {
+    Command::new(command).arg(arg).spawn()?;
+    Ok(())
+}
+
+// ----- Minor Helper Functions -----
+// Get the &str representation of a path.
+fn path_str(path: &PathBuf) -> &str {
+    path.to_str().expect("Path has invalid unicode")
+}
+
+// Return the month name of valid month number.
 fn month(num: u32) -> String {
     match num {
         1 => "January",
